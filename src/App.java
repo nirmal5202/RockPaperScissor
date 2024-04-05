f55eb51cbdad55bea5a55398e1553cc5842b2dfe
nirpatel <nirpatel@trentu.ca>
2024-04-05 09:57:46 -0400

    main

--- src/App.java
+++ src/App.java
@@ -1,15 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class App extends JFrame{
 
-    public App(){
-        setTitle("Rock >?< Paper >?< Scissor");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // setLayout(new BorderLayout());
-        setLayout(new FlowLayout());
-
-    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
